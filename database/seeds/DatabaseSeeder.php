0a2e85f2a56396a6f01202aa04422cca1dd74a1e
Rodrigo Banci <rblima0@gmail.com>
2016-11-04 18:05:22 -0200

    Seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,19 +2,26 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use estoque\Categoria;
 
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
 	public function run()
 	{
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('CategoriaTableSeeder');
+	}
+
+}
+
+class CategoriaTableSeeder extends Seeder {
+
+	public function run (){
+		Categoria::create(['nome' => 'INFORMATICA']);
+		Categoria::create(['nome' => 'HARDWARE']);
+		Categoria::create(['nome' => 'ELETRODOMESTICO']);
+		Categoria::create(['nome' => 'PERIFERICOS']);
 	}
 
 }
